2314799bc349788ec894acbff3d69dab09d3b597
Ary lucas alves da silva <arylucas8@gmail.com>
2026-01-10 16:40:03 -0300

    feat: add support to use Link from next

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Sidebar(){
-  return(
-    <aside className="w-64 bg-zinc-900 text-white h-screen p-4">
+export default function Sidebar() {
+  return (
+    <aside className="w-64 bg-zinc-900 text-white h-auto p-4">
       <div className="flex items-center mb-4 gap-4">
         <Image
           className="dark:invert"
@@ -12,14 +13,30 @@ export default function Sidebar(){
           height={30}
           priority
         />
-        <h1>Nome do Usuário</h1>
+        <h1>remyklo</h1>
       </div>
 
-      <nav className="flex flex-col gap-4 mt-8">
-        <button className="text-left cursor-pointer">Hoje</button>
-        <button className="text-left cursor-pointer">Em breve</button>
-        <button className="text-left cursor-pointer">Projetos</button>
+      <nav className="flex flex-col h-screen justify-between">
+        <div className="flex flex-col gap-4 mt-8">
+          <Link href={"/tarefas/hoje"} className="text-left cursor-pointer">Hoje</Link>
+          <Link href={"/tarefas/em-breve"} className="text-left cursor-pointer">Em breve</Link>
+          <Link href={"/tarefas/projetos"} className="text-left cursor-pointer">Projetos</Link>
+          <Link href={"/tarefas/arquivados"} className="text-left cursor-pointer">Arquivados</Link>
+        </div>
+        <div className="flex items-center mb-4 mt-auto justify-center">
+          <Image
+            className="dark:invert"
+            src="/trash.svg"
+            alt="trash bin"
+            width={60}
+            height={60}
+            priority
+          />
+
+          <Link href={"/lixeira"} className="text-center cursor-pointer">Lixeira</Link>
+        </div>
       </nav>
+
     </aside>
   )
 }
