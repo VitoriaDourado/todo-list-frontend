79c532350b82756aa7eaab1c6013408891cb6334
Ary lucas alves da silva <arylucas8@gmail.com>
2026-01-10 16:40:03 -0300

    feat: add support to use Link from next

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 import { X, Menu } from "lucide-react";
+
 
 interface SidebarProps {
   isOpen: boolean;
@@ -9,9 +11,8 @@ interface SidebarProps {
 export default function Sidebar({ isOpen, toggle }: SidebarProps) {
   return (
     <aside
-      className={`bg-zinc-900 text-white h-screen p-4 transition-all duration-300 ${
-        isOpen ? "w-64" : "w-16"
-      }`}
+      className={`bg-zinc-900 text-white h-screen p-4 transition-all duration-300 ${isOpen ? "w-64" : "w-16"
+        }`}
     >
       <button onClick={toggle} className="mb-6 cursor-pointer">
         {isOpen ? <X /> : <Menu />}
@@ -19,23 +20,40 @@ export default function Sidebar({ isOpen, toggle }: SidebarProps) {
 
       {isOpen && (
         <>
-          <div className="flex items-center gap-4 mb-8">
+          <div className="flex items-center mb-4 gap-4">
             <Image
+              className="dark:invert"
               src="/globe.svg"
-              alt="Logo"
+              alt="Next.js logo"
               width={30}
               height={30}
+              priority
             />
-            <h1>Nome do Usuário</h1>
+            <h1>remyklo</h1>
           </div>
 
-          <nav className="flex flex-col gap-4">
-            <button className="text-left">Hoje</button>
-            <button className="text-left">Em breve</button>
-            <button className="text-left">Projetos</button>
+          <nav className="flex flex-col h-screen justify-between">
+            <div className="flex flex-col gap-4 mt-8">
+              <Link href={"/tarefas/hoje"} className="text-left cursor-pointer">Hoje</Link>
+              <Link href={"/tarefas/em-breve"} className="text-left cursor-pointer">Em breve</Link>
+              <Link href={"/tarefas/projetos"} className="text-left cursor-pointer">Projetos</Link>
+              <Link href={"/tarefas/arquivados"} className="text-left cursor-pointer">Arquivados</Link>
+            </div>
+            <div className="flex items-center mb-4 mt-auto justify-center">
+              <Image
+                className="dark:invert"
+                src="/trash.svg"
+                alt="trash bin"
+                width={60}
+                height={60}
+                priority
+              />
+
+              <Link href={"/lixeira"} className="text-center cursor-pointer">Lixeira</Link>
+            </div>
           </nav>
         </>
       )}
     </aside>
-  );
+  )
 }
