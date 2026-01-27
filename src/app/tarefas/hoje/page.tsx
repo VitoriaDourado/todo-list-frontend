8f867870120ce02dc4b0f704d154f5506df71e95
Vitória Dourado <vickawaii14@gmail.com>
2026-01-26 21:53:02 -0300

    :sparkles: feat: Adding dates in tasks for show to pages

--- src/app/tarefas/hoje/page.tsx
+++ src/app/tarefas/hoje/page.tsx
@@ -1,8 +1,33 @@
+'use client'
 
-export default function Hoje() {
+import { TaskList } from '../../../components/tasks/TaskList'
+import { useTasks } from '../../../hooks/useTasks'
+import { isToday } from '../../../services/date.utils'
+
+export default function HojePage() {
+  const { tasks, loading, toggleTask, removeTask } = useTasks()
+
+  const todayTasks = tasks.filter(task =>
+    isToday(task.createdAt)
+  )
+
   return (
-    <div className="flex h-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Página hoje
+    <div>
+      <div className="bg-black text-white flex items-center justify-center pt-5">
+        Minhas tasks de hoje
+      </div>
+
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+        {loading ? (
+          <p>Carregando...</p>
+        ) : (
+          <TaskList
+            tasks={todayTasks}
+            onToggle={toggleTask}
+            onRemove={removeTask}
+          />
+        )}
+      </div>
     </div>
-  );
+  )
 }
